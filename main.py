f0b224f6b7b4873ab21b7507359360e26890cab8
Digu <2919408342@qq.com>
2022-08-22 23:16:47 +0800

    https://github.com/DIGUZHS/WeChat_daily.git

---
+++ main.py
@@ -0,0 +1,56 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+today = datetime.now()
+todays = today.strftime('%Y-%m-%d %A')
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthday = os.environ['BIRTHDAY']
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+user_id = os.environ["USER_ID"]
+#user_ids = [os.environ["USER_ID_1"], os.environ["USER_ID_2"]]
+
+template_id = os.environ["TEMPLATE_ID"]
+
+
+def get_weather():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['weather'], math.floor(weather['low']),math.floor(weather['high'])
+
+def get_count():
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+def get_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_words():
+  words = requests.get("https://api.shadiao.pro/pyq")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea, low, high = get_weather()
+data = {"date":{"value":todays}, "city":{"value":city}, "weather":{"value":wea},"min_temperature":{"value":low,"color":"#7FBA00"},"max_temperature":{"value":high, "color":"#F25022"}, "birthday":{"value":get_birthday(),"color":"#FF0000"},"note":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
